abcee0a600f04937c81442ea0313a3a6e3b66267
akhilkasturi <117470270+akhilkasturi@users.noreply.github.com>
2024-10-08 18:37:30 -0400

    Add files via upload
    Proof of Concept: Python code using Pygame to create a game window that can be opened and closed

---
+++ CatchTheBus.py
@@ -0,0 +1,30 @@
+import os
+import random
+import math
+import pygame
+from os import listdir
+from os.path import isfile, join
+pygame.init()
+
+pygame.display.set_caption("Catch the Bus")
+WIDTH, HEIGHT = 1000, 600
+FPS = 60
+window = pygame.display.set_mode((WIDTH,HEIGHT))
+
+def main(window):
+    clock = pygame.time.Clock()
+
+    run = True
+    while run:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+    
+    pygame.quit()
+    quit()
+
+if __name__ == "__main__":
+    main(window)
